5cf4b3aa55d5fd53d050a71e4a8d4ec089b5878e
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-03-15 09:04:14 -0300

    15/03/2023

--- Introduction/playing-with-char.c
+++ Introduction/playing-with-char.c
@@ -3,8 +3,11 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() 
-{
+int main(void) {
+
+    char ch, s[100], sen[100];
+    scanf("%c\n%s\n%[^\n]%*c\n", &ch, s, sen);
+    printf("%c\n%s\n%s\n", ch, s, sen);
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
